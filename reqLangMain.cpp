af1927635cca0af77d4a56b1ca952d1e78830cea
srod5125 <srod5125@gmail.com>
2023-08-06 22:09:23 -0600

    setting up main cpp file, added todos

---
+++ reqLangMain.cpp
@@ -0,0 +1,15 @@
+
+
+
+// 
+int main() {
+    return 0;
+}
+
+
+// g++ -g -std=c++20 $(find . -type f -iregex ".*\.cpp") -o interpreter
+// clang++ -g -std=c++20 $(find . -type f -iregex ".*\.cpp") -o interpreter
+
+// TODO 1: write parser with antlr
+// TODO 2: interop with json
+// TODO 3: outline structer of lang, write searcher
